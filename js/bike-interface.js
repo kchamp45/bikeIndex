e05d42ae3b3034932bc314a2db67b2cf98e85713
Kimberly Lu <kimlu4@comcast.net>
2017-09-13 22:08:19 -0700

    edit UI and B/E logics

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -15,31 +15,40 @@ import { Bike } from './../js/bike.js';
 //     bike.getStatus(userInput, displayStatus);
 //   });
 // });
+  let displayStatus = function(bikes) {
+      $('#notStolen').text(bikes.stolen);
+    };
+
 $(document).ready(function(){
+  let bike = new Bike();
   $('#serial-form').submit(function(){
     let serialNum = $("#unique").val();
-
-    let request = new XMLHttpRequest();
-    let url = `https://bikeindex.org:443/api/v3/search?serial=${serialNum}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200){
-        alert("hey");
-        let response = JSON.parse(this.responseText);
-        console.log(responseText);
-        console.log(response);
-        getStatus(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    getStatus = function(response) {
-      // $("#bike-serial").text(serialNum);
-      if (response.body.bikes.stolen === false){
-        $("#notStolen").text("not");
-      }
-    };
+    bike.getStatus(serialNum, displayStatus);
+    $('#bike-serial').text(serialNum);
   });
 });
+
+    // let request = new XMLHttpRequest();
+    // let url = `https://bikeindex.org/api/v3/search?serial=${serialNum}`;
+    //
+    // request.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200){
+    //     alert("hey");
+    //     let response = JSON.parse(this.responseText);
+    //     console.log(responseText);
+    //     console.log(response);
+    //     getStatus(response);
+    //   }
+    // };
+    //
+    // request.open("GET", url, true);
+    // request.send();
+    //
+    // getStatus = function(response) {
+    //   // $("#bike-serial").text(serialNum);
+    //   if (response.body.bikes.stolen === false){
+    //     $("#notStolen").text("not");
+    //   }
+    // };
+//   });
+// });
--- js/bike.js
+++ js/bike.js
@@ -1,5 +1,17 @@
 export class Bike {
+  constructor(serialNumber) {
+    this.serialNumber = serialNumber;
+  }
 
+  getStatus(serialNum, displayStatus) {
+    $.get(`https://bikeindex.org/api/v3/search?serial=${this.serialNumber}`)
+      .then(function(bikes) {
+        displayStatus(bikes);
+      })
+      .fail(function() {
+        console.log('Try again');
+      });
+  }
   // getStatus(serialNum, displayStatus) {
   //   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&serial='+serialNum)
   //     .then(function(results) {
